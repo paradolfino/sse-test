a5a02a9c3669ca950010675e41e5dc8fcfe52355
Viktharien <viktharien@zoho.com>
2018-04-17 00:13:03 +0000

    initial project setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello/index'
+  root 'hello#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
