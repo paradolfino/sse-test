b589ca006b22b5ca6a69b981b4de84aacea00bb8
Viktharien <viktharien@zoho.com>
2018-04-17 00:32:52 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,11 +1,11 @@
 class HelloController < ApplicationController
   include ActionController::Live
   def index
-    @last = nil
+    @last = Transaction.last
     loop do
       @transaction = Transaction.last
       @transactions = Transaction.all
-      if @transactions.changed?
+      if @transactions
         response.stream.write "#{@transaction.content} <br/>"
       end
       sleep 2
