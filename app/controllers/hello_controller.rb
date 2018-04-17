724c2a7185b5f2fb362c510060d852fcf8c5485b
Viktharien <viktharien@zoho.com>
2018-04-17 00:26:05 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,7 +3,7 @@ class HelloController < ApplicationController
   def index
     @transaction = Transaction.last
     5.times do
-      response.stream.write "World <br/>"
+      response.stream.write @transaction.content
       sleep 2
     end
     response.stream.close
