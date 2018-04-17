ddb7683c91ca84a631a4790cf5d31d69059fe020
Viktharien <viktharien@zoho.com>
2018-04-17 00:43:44 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,8 +1,17 @@
 class HelloController < ApplicationController
   include ActionController::Live
-  def index
-    @last = Transaction.last
-    @transaction = Transaction.last
-    
+  def ticker
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = Ticker::SSE.new(response.stream)
+    begin
+      loop do
+        sse.write({ time: Time.now })
+        sleep 2
+      end
+    rescue IOError
+      logger.info 'Client disconnects causes IOError on write'
+    ensure
+      sse.close
+    end
   end
 end
