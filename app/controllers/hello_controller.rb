008bdc65125d77c93421247c9721be6ac1c49d80
Viktharien <viktharien@zoho.com>
2018-04-17 00:28:08 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,7 +2,9 @@ class HelloController < ApplicationController
   include ActionController::Live
   def index
     @transaction = Transaction.last
-    5.times do
+    @transactions = Transaction.all
+    loop do
+      if @transaction
       response.stream.write @transaction.content
       sleep 2
     end
