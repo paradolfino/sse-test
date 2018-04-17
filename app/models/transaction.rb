45d00a4c8b90a28b1c4296721e1883aa245aff39
Viktharien <viktharien@zoho.com>
2018-04-17 01:00:23 +0000

    initial project setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,32 +1,4 @@
 class Transaction < ApplicationRecord
     include ActiveModel::Dirty
-    define_attribute_methods :content
-    def initialize
-        @content = nil
-    end
     
-    def content
-        @content
-    end
-    
-    def content=(val)
-        content_will_change! unless val == @content
-        @content = val
-    end
-    
-    def save
-        # do persistence work
-    
-        changes_applied
-    end
-    
-    def reload!
-        # get the values from the persistence layer
-    
-        clear_changes_information
-    end
-    
-    def rollback!
-        restore_attributes
-    end
 end
