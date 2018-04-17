b924e7605ba3b32944e251df97316b8f4c096154
Viktharien <viktharien@zoho.com>
2018-04-17 00:23:54 +0000

    initial project setup

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,4 +6,10 @@ class TransactionsController < ApplicationController
     def create
         
     end
+    
+    private
+        
+        def transaction_params
+           params.require(:transaction).permit(:content) 
+        end
 end
