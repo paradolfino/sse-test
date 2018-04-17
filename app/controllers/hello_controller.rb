89dc5b328fff3871bda5ea2f54283d575215f255
Viktharien <viktharien@zoho.com>
2018-04-17 00:12:41 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,10 @@
 class HelloController < ApplicationController
+  include ActionController::Live
   def index
+    5.times do
+      response.stream.write "hi <br/>"
+      sleep 2
+    end
+    response.stream.close
   end
 end
