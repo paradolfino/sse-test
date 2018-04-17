63988c605ec9ef898b3fe35044f823440bbcccc8
Viktharien <viktharien@zoho.com>
2018-04-17 00:54:03 +0000

    initial project setup

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,10 @@
 class HelloController < ApplicationController
   include ActionController::Live
   def ticker
-    @trans = Transaction.last
-    @trans_last = @trans
     response.headers['Content-Type'] = 'text/event-stream'
     sse = Ticker::SSE.new(response.stream)
     begin
       loop do
-        @trans = Transaction.last
         if @trans != @trans_last
           sse.write({ time: Time.now })
           sleep 2
